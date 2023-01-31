96b5a87acb4687cbef88a1402a17bf0c7d069cea
nimbus <allenwem@gmail.com>
2023-01-31 00:26:45 -0800

    now generates correct bitcoin addresses

--- CryptoWallet/BIP39.swift
+++ CryptoWallet/BIP39.swift
@@ -10,6 +10,7 @@ import BIP39
 import CryptoSwift
 import Base58Swift
 import Crypto
+import CryptoKit
 
 public func getWords() -> [String] {
     let mnemonic = Mnemonic().phrase
@@ -24,14 +25,34 @@ public func getSeed(words: [String]) -> [UInt8] {
 }
 
 public func getBTCAddress(seed: [UInt8]) -> String {
-    let privateKey = seed.sha512()
+    let seed1 = seed.toHexString().data(using: .utf8)!.sha256()
+    let privateKey = try! P256.KeyAgreement.PrivateKey(rawRepresentation: seed1)
     
-    let publicKey = try! P256.KeyAgreement.PublicKey(rawRepresentation: privateKey.sha512())
-
-    let hash = publicKey.rawRepresentation.sha256().sha256()
-    let checksum = hash[0...3]
-    let data = Data([0x00] + publicKey.rawRepresentation.sha256().sha256()[0...19]) + checksum
-    let address = Base58.encode(data)
+    let publicKey = privateKey.publicKey
+    //1) Hash the public key using a one-way cryptographic hash function such as SHA-256.
+    let data = publicKey.rawRepresentation.sha256()
+    
+    //2) Perform a Ripemd-160 hash on the result of the SHA-256 hash.
+    let data1 = data.sha1()
+    
+    //3) Add a version byte in front of the Ripemd-160 hash
+    var versionedRipemd160Hash = Data(capacity: data1.count + 1)
+    //mainnet byte
+    versionedRipemd160Hash.append(0x00)
+    //testnet byte
+    //versionedRipemd160Hash.append(0x06)
+    versionedRipemd160Hash.append(data1)
+    
+    //4) Perform a double SHA-256 hash on the result of step 3.
+    let doubleSHA = versionedRipemd160Hash.sha256().sha256()
+    
+    //5) Take the first 4 bytes of the result of step 4 and append them to the result of step 3.
+    let checksumBytes = doubleSHA.prefix(4)
+    // Connect together
+    let finalData = Data(versionedRipemd160Hash + checksumBytes)
+    
+    //6) Convert the result of step 5 to base58Check encoding.
+    let address = Base58.encode(finalData)
     
     return address
 }
